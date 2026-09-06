83643f23d416b0bd72bb2d5320951045f674cd88
Gustavo1475 <161898660+Gustavo1475@users.noreply.github.com>
2026-09-05 21:19:05 -0300

    Ícone da aba
    Mudança do ícone da aba de navegação

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -98,7 +98,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
         rel: "stylesheet",
         href: appCss,
       },
-      { rel: "icon", href: "/favicon.ico", type: "image/x-icon" },
+      { rel: "icon", href: "public/favicon.ico", type: "image/x-icon" },
     ],
   }),
   shellComponent: RootShell,
